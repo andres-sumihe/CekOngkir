2629d106c4a1def89f6a83ad48defc2fd3d1cc57
andres-sumihe <672018136@student.uksw.edu>
2021-03-08 22:33:20 +0900

    Test OkHttp Request

--- app/src/main/java/com/andflow/cekongkir/MainActivity.java
+++ app/src/main/java/com/andflow/cekongkir/MainActivity.java
@@ -3,12 +3,54 @@ package com.andflow.cekongkir;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "thttps://api.rajaongkir.com/starter/province";
+
+        Request request = new Request.Builder().url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String myResponse = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewResult.setText(myResponse);
+                        }
+                    });
+                }
+            }
+        });
+
+
     }
 }
